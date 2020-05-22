c15d889f3a5dd36b124ee30d533dad67e1e3bbec
unknown <kamila0990@gmail.com>
2020-05-22 16:56:06 +0100

    Updates

---
+++ precache-manifest.221c6bd23e7fc768ff63e2f34d8ee0cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65a0c47f922b37bb985c1d73c9315986",
+    "url": "/Travel-Notes/index.html"
+  },
+  {
+    "revision": "c20d03ebf91324c32d86",
+    "url": "/Travel-Notes/static/css/main.2ed5a44a.chunk.css"
+  },
+  {
+    "revision": "4fea54ed2fb122550bf0",
+    "url": "/Travel-Notes/static/js/2.88b91a90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Travel-Notes/static/js/2.88b91a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c20d03ebf91324c32d86",
+    "url": "/Travel-Notes/static/js/main.3b87a27a.chunk.js"
+  },
+  {
+    "revision": "aba7ae83afd2fe5c6df5",
+    "url": "/Travel-Notes/static/js/runtime-main.fb01cdd4.js"
+  },
+  {
+    "revision": "f6e551d8b78c0906534952ffc6e8f357",
+    "url": "/Travel-Notes/static/media/1976241.f6e551d8.jpg"
+  },
+  {
+    "revision": "3e77918da37787c370efca484b0985f0",
+    "url": "/Travel-Notes/static/media/Logo.3e77918d.png"
+  }
+]);
