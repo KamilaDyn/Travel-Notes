c09bf71c035b30e27443f075601e42fb8820d07d
Kamila <kamila.dynysiuk@gmail.com>
2020-09-29 15:47:58 +0100

    add # to router links

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,5 +1,11 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, {
+  Component
+} from "react";
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from "react-router-dom";
 import Header from "../.././components/Header/Header";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import AppContext from "../../context";
@@ -26,19 +32,28 @@ class Root extends Component {
 
   menuClicked = () => {
     this.setState((prevState) => {
-      return { isSidebarOpen: !prevState.isSidebarOpen, display: "block " };
+      return {
+        isSidebarOpen: !prevState.isSidebarOpen,
+        display: "block "
+      };
     });
     console.log("works");
   };
   closeSidebar = () => {
-    this.setState({ isSidebarOpen: false });
+    this.setState({
+      isSidebarOpen: false
+    });
   };
 
   openModal = () => {
-    this.setState({ isModalOpen: true });
+    this.setState({
+      isModalOpen: true
+    });
   };
   closeModal = () => {
-    this.setState({ isModalOpen: false });
+    this.setState({
+      isModalOpen: false
+    });
   };
 
   addItem = (e, newItem) => {
@@ -62,42 +77,90 @@ class Root extends Component {
     let backdrop;
 
     if (this.state.isSidebarOpen || this.state.isModalOpen) {
-      backdrop = (
-        <Backdrop click={this.closeSidebar} display={this.state.display} />
+      backdrop = ( <
+        Backdrop click = {
+          this.closeSidebar
+        }
+        display = {
+          this.state.display
+        }
+        />
       );
     }
 
-    const { isModalOpen, isSidebarOpen } = this.state;
+    const {
+      isModalOpen,
+      isSidebarOpen
+    } = this.state;
     const contextElements = {
       ...this.state,
       addItem: this.addItem,
     };
 
-    return (
-      <AppContext.Provider value={contextElements}>
-        <BrowserRouter>
-          <div className={styles.wrapper}>
-            <Sidebar closeSidebarFn={this.closeSidebar} show={isSidebarOpen} />
-            {backdrop}
-            <div className={styles.rightWrapper}>
-              <Header
-                openModalFn={this.openModal}
-                showSidebarFn={this.menuClicked}
-              />
-              <Switch>
-                <Route exact path="/" component={Instagram} />
-                <Route path="/list" component={List} />
-                <Route path="/links" component={Links} />
-              </Switch>
+    return ( <
+      AppContext.Provider value = {
+        contextElements
+      } >
+      <
+      BrowserRouter >
+      <
+      div className = {
+        styles.wrapper
+      } >
+      <
+      Sidebar closeSidebarFn = {
+        this.closeSidebar
+      }
+      show = {
+        isSidebarOpen
+      }
+      /> {
+        backdrop
+      } <
+      div className = {
+        styles.rightWrapper
+      } >
+      <
+      Header openModalFn = {
+        this.openModal
+      }
+      showSidebarFn = {
+        this.menuClicked
+      }
+      /> <
+      Switch >
+      <
+      Route exact path = "/#/"
+      component = {
+        Instagram
+      }
+      /> <
+      Route path = "/#/list"
+      component = {
+        List
+      }
+      /> <
+      Route path = "/#/links"
+      component = {
+        Links
+      }
+      /> <
+      /Switch>
 
-              <Button onClick={this.openModal}>+</Button>
-              {isModalOpen && <Modal closeModalFn={this.closeModal} />}
-            </div>
-          </div>
-        </BrowserRouter>
-      </AppContext.Provider>
-    );
+      <
+      Button onClick = {
+        this.openModal
+      } > + < /Button> {
+        isModalOpen && < Modal closeModalFn = {
+          this.closeModal
+        }
+        />} <
+        /div> <
+        /div> <
+        /BrowserRouter> <
+        /AppContext.Provider>
+      );
+    }
   }
-}
 
-export default Root;
+  export default Root;
